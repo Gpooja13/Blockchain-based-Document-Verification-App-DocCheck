06dc716901a0d00e9b4094bd6953b31c6baa99d6
Gpooja13 <145600866+Gpooja13@users.noreply.github.com>
2024-11-06 16:26:09 +0530

    edited login and signup

--- frontend/src/Screens/SignIn.jsx
+++ frontend/src/Screens/SignIn.jsx
@@ -44,7 +44,7 @@ const SignIn = () => {
       <Background />
       <div className="absolute top-0 left-0 bg-black bg-opacity-60 h-screen w-screen ">
         <div className="flex flex-col items-center justify-center gap-8 h-[100%]">
-          <div className="flex flex-col items-center justify-center p-8 bg-white bg-opacity-90 text-white w-96 rounded-3xl ">
+          <div className="flex flex-col items-center justify-center p-8 bg-white bg-opacity-90 text-white w-[27rem] rounded-3xl ">
             <h1 className="text-4xl font-semibold text-black">
               <div className="flex items-center">
                 <img src="/logo.webp" alt="" width={"60px"} />
@@ -66,7 +66,7 @@ const SignIn = () => {
                     [e.target.name]: e.target.value,
                   })
                 }
-                className="px-4 py-2 w-72 border border-gray-500 bg-gray-800 text-white rounded"
+                className="px-4 py-2 w-80 border border-gray-500 bg-gray-800 text-white rounded"
               />
               <input
                 type="password"
@@ -79,7 +79,7 @@ const SignIn = () => {
                     [e.target.name]: e.target.value,
                   })
                 }
-                className="px-4 py-2 w-72 border border-gray-500 bg-gray-800 text-white rounded"
+                className="px-4 py-2 w-80 border border-gray-500 bg-gray-800 text-white rounded"
               />
               <p className="text-red-500 text-sm absolute bottom-60">
                 {errorLogin}
